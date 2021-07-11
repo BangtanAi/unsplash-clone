568ff7809f0acfa5338a5353fcf50d4b6abfff1f
BangtanAi <kairova-94@mail.ru>
2021-07-12 00:01:22 +0600

    при нажатии на кнопку открывается фото

--- src/App.js
+++ src/App.js
@@ -1,33 +1,81 @@
 import axios from 'axios';
 import React from 'react';
 import './index.scss';
+import Pagination from '@material-ui/lab/Pagination';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Photo from './Photo';
 
 function App() {
-  const[value, setValue] = React.useState("");
-  const[results, setResults] = React.useState([])
-  const fetchImages=()=>{
-    fetch(`https://api.unsplash.com/search/photos?query=&client_id=gLSSdfKNMYT6TQr4WU_bBzmf0r5FwHqRkgCqTd9I6Co&query=${value}&orientation=squarish&per_page=20`)
-    .then(res=>res.json())
-    .then(data=>{
-      console.log(data)
-      setResults(data.results)
-    })
+  const [text, setText] = React.useState('');
+  const [loading, setLoading] = React.useState(false);
+  const [results, setResults] = React.useState([]);
+  const [page, setPage] = React.useState();
+  const [photoOpened, setPhotoOpened] = React.useState(false);
+  const [photo, setPhoto] = React.useState();
+  
+  const onPhotoClick = (photo) => {
+    setLoading(true);
+    setPhoto(photo);
+    setPhotoOpened(true);
+    setLoading(false);
+  }
+
+  const fetchImages = async () => {
+    setLoading(true);
+    const response = await axios.get(
+      `https://api.unsplash.com/search/photos?page=${page}&query=${text}&client_id=gLSSdfKNMYT6TQr4WU_bBzmf0r5FwHqRkgCqTd9I6Co&orientation=squarish&order_by=popular`,
+    );
+    const data = await response.data;
+    console.log(results.length);
+    setResults(data.results);
+    console.log(data.total);
+    setLoading(false);
+  };
+  const pageChange = (event, value) => {
+    setPage(value);
+    fetchImages(text, value);
+  };
+  React.useEffect(() => {
+    fetchImages();
+  }, []);
+  if (loading) {
+    return (
+      <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+      >
+        <CircularProgress size={130} />
+      </div>
+    );
   }
   return (
     <div className="App">
+      {photoOpened && <Photo setPhotoOpened={setPhotoOpened} photo={photo} />}
       <div className="searchBlock">
-        {/* <span>Search:</span> */}
-        <input 
-        type="text"
-        value={value}
-        onChange={(e)=>setValue(e.target.value)} />
+        <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
         <button onClick={fetchImages}>Send</button>
       </div>
+      {results.length > 0 ? (
+        <div className="pagination">
+          <Pagination
+            count={10}
+            variant="outlined"
+            color="primary"
+            size="large"
+            onChange={pageChange}
+            page={page}
+          />
+        </div>
+      ) : null}
+
       <div className="gallery">
-        {results.map((item, id)=>{
-          return <img className="item" key={id} src={item.urls.regular} alt="galleryImg" />
-        })
-        }
+        {results.map((item, id) => {
+          return <img onClick={()=>onPhotoClick(item.urls.regular)} className="item" key={id} src={item.urls.small} alt="galleryImg" />;
+        })}
       </div>
     </div>
   );
---
+++ src/Photo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Photo({ setPhotoOpened, photo }) {
+  return (
+    <div className="overlay">
+        <img className="photoUrl" src={photo} alt="photourl" />
+        <img className="removeBtn" onClick={() => setPhotoOpened(false)} src="/img/btn-remove.svg" alt="photoOpen" />
+    </div>
+  );
+}
+
+export default Photo;
